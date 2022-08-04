7de72ce42dc2bcb7933326711a916784dd6cf61c
harshab1 <harshabashetty1@gmail.com>
2022-08-04 20:35:55 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # streaming-gcs-to-bq
-Streaming data from Cloud Storage into Bigquey using Cloud Functions
+Streaming data from Cloud Storage into BigQuery using Cloud Functions
+
+## GCP Services:
+### Cloud Functions: 
+It is an event-driven, serverless compute platform, which provides automatic scaling, high availability and fault tolerance with no servers to provision, manage, update, or patch.
+
+### Firestore: 
+It is a NoSQL serverless document database that simplifies storing, syncing, and querying data for mobile, web and IoT apps at global scale. 
+
+
+
+## Steps:
+As seen in the Architecture diagram, the pipeline consists of following steps:
+
+1. JSON files are uploaded to the `FILES_SOURCE` Cloud Storage Bucket 
+2. This event triggers the `streaming` Cloud Function
+3. Data is parsed and inserted into BigQuery
+4. The ingestion status is logged into Firestore and Cloud Logging
+5. A message is published in one of the following Pub/Sub topics:
+- `streaming_success_topic`
+- `streaming_error_topic`
+6. Depending on the results, Cloud Functions moves the JSON file from the `FILES_SOURCE` bucket to one of the following buckets:
+- `FILES_ERROR`
+- `FILES_SUCCESS`
+
+## Objectives:
+1. Create a Cloud Storage bucket to store the JSON files
+2. Create a BQ dataset and table to stream the data in to
+3. Configure a Cloud Function to trigger whenever files are added to the bucket
+4. Set up Pub/Sub topics
+5. Configure additional functions to handle function output
+6. Test the streaming pipeline
+7. Configure Cloud Monitoring to alert on any unexpected behaviors
+
+
+## Useful links:
+- Original project: https://cloud.google.com/architecture/streaming-data-from-cloud-storage-into-bigquery-using-cloud-functions
+- Code repo: https://github.com/GoogleCloudPlatform/solutions-gcs-bq-streaming-functions-python
+- Firestore documentation: https://github.com/GoogleCloudPlatform/python-docs-samples/blob/909a077e4565ac55ce9a6dd3e21e9991b1f76fce/firestore/cloud-client/snippets.py#L173-L189
+- Blob (download_as_string): https://googleapis.dev/python/storage/latest/blobs.html
+
+
+
+
+
+
---
+++ functions/streaming/main.py
@@ -0,0 +1,110 @@
+'''This Cloud Function is responsible for:
+- Parsing and validating new files added to Cloud Storage
+- Checking for duplications
+- Inserting files content into Bigquery
+- Logging ingestion status into Cloud Firestore and Stackdriver
+- Publishing a message to either an error or success topic in Cloud Pub/Sub
+'''
+
+import logging
+from datetime import datetime
+import pytz
+import json
+import os
+import traceback
+
+from google.cloud import firestore
+from google.cloud import storage
+from google.cloud import bigquery
+from google.api_core import retry
+from google.cloud import pubsub_v1
+
+
+PROJECT_ID = os.getenv('GCP_PROJECT')
+BQ_DATASET = 'mydataset'
+BQ_TABLE = 'mytable'
+DB = firestore.Client()
+CS = storage.Client()
+BQ = bigquery.Client()
+PS = pubsub_v1.Client()
+ERROR_TOPIC = '/projects/%s/topics/%s' % (PROJECT_ID, 'streaming_error_topic')
+SUCCESS_TOPIC = '/projects/%s/topics/%s' % (PROJECT_ID, 'streaming_success_topic')
+
+
+def streaming(data, context):
+    '''This function is executed whenever a file is added to Cloud Storage. Flow of logic in this function:
+    -  Checks if the data is already ingested
+    - If yes, handles duplication
+    - Else, Ingested into BQ and successful message is published to Pub/Sub streaming_error_topic topic
+    - In case of exception, unsuccessful message is published to Pub/Sub streaming_success_topic
+     '''
+    bucket_name = data['bucket']
+    file_name = data['name']
+    db_ref = DB.document(u'streaming_files/%s' % file_name)
+    if _was_already_ingested(db_ref):
+        _handle_duplication(db_ref)
+    else:
+        try:
+            _insert_into_bigquery(bucket_name, file_name)
+            _handle_success(db_ref)
+        except Exception:
+            _handle_error(db_ref)
+
+
+def _was_already_ingested(db_ref):
+    status = db_ref.get()
+    if status.exists:
+        return True
+    else:
+        return False
+
+def _handle_duplication(db_ref):
+    dups = [_now()]
+    data = db_ref.get().to_dict()
+    if 'duplication_attempts' in data:
+        dups.extend(data['duplication-attempts'])
+    db_ref.update({
+        'duplication_attempts': dups
+    })
+    logging.warning('Duplication attempt while streaming file \'%s\'' % db_ref.id)
+
+def _now():
+    datetime.utcnow().replace(tzinfo=pytz.utc).strftime('%Y-%m-%d %H:%M:%S %Z')
+
+def _insert_into_bigquery(bucket_name, file_name):
+    blob = CS.get_bucket(bucket_name).blob(file_name)
+    row = json.loads(blob.download_as_string())
+    table = BQ.dataset(BQ_DATASET).table(BQ_TABLE)
+    errors = BQ.insert_rows_json(table, 
+                                json_row=[row], 
+                                row_ids=[file_name],
+                                retry=retry.Retry(deadline=30))
+    # if errors != []:
+    #     raise BigQueryError(errors)
+
+def _handle_success(db_ref):
+    message = 'File \'%s\' streamed into BigQuery' % db_ref.id
+    doc = {
+        u'success': True,
+        u'when': _now()
+    }
+    db_ref.set(doc)
+    PS.publish(SUCCESS_TOPIC, message.emcode('utf-8', file_name=db_ref.id))
+    logging.info(message)
+
+def _handle_error(db_ref):
+    message = 'Error streaming file \'%s\. Cause: %s' % (db_ref.id, traceback.format_exec())
+    doc = {
+        u'success': False,
+        u'error_message': message,
+        u'when': _now()
+    }
+    db_ref.set(doc)
+    PS.publish(ERROR_TOPIC, message.encode('utf-8'), file_name=db_ref.id)
+    logging.error(message)
+
+# class BiqQueryError(Exception):
+
+
+
+
